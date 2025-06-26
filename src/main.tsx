f6399c6bcbe47479707966e38eddc638b9246e83
winclip <sokhotnikoff@gmail.com>
2025-06-26 22:58:09 +0200

    style: add global CSS reset and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { store } from "./store/store";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
